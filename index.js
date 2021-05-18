df6238d2a6ed1ba192986d211abee12fe8ab9546
Prashant Joshi <prashant.joshi@softobiz.com>
2021-05-18 14:59:49 +0530

    Initial Commit two files added index.html, index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+console.log("this is my first line of code in javascript");
+let name = "Prashant Joshi";
+console.log("My name is "+name);
+alert("hey live server is working");
