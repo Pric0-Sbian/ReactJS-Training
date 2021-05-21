4cc650407c9abf6ae0fe7aaafeb51fe1cc270491
Prashant Joshi <prashant.joshi@softobiz.com>
2021-05-21 13:59:25 +0530

    solved the function callback problem

--- src/callback func.ts
+++ src/callback func.ts
@@ -1,29 +1,27 @@
-type numstr = number|string;
+type numstr = number | string;
 function add(
-    num1: numstr,        
-    num2: numstr,
-    cb: (num:numstr) => void)
-{
-    if(typeof(num1) === 'number' && typeof(num2) === 'number')
-    {
-        let ans = num1 + num2;
-        return ans;
-        cb(ans);
-    } 
-    else if(typeof(num1) === 'string' && typeof(num2) === 'string')
-    {
-        let ans = num1 + num2;
-        return ans;
-        cb(ans);
-    }
-    else 
-    {
-        console.log("Please enter same type of data");
-    }
+  num1: numstr,
+  num2: numstr,
+  cb: (num: numstr) => void) {
+  if (typeof (num1) === 'number' && typeof (num2) === 'number') {
+    let ans = num1 + num2;
+    cb(ans);
+    return ans;
+
+  }
+  else if (typeof (num1) === 'string' && typeof (num2) === 'string') {
+    let ans = num1 + num2;
+    cb(ans);
+    return ans;
+
+  }
+  else {
+    console.log("Please enter same type of data");
+  }
 
 }
-console.log(add(15, 20, (result) => {
-    console.log(result);
-    return result;
-}));
+add(15, 20, (result) => {
+  console.log(result);
+  //return result;
+});
 console.log("hey guyssssssss");
