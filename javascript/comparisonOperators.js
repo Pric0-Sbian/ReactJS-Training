6f00ba658e5cb0e3647bc316ae8bf94a7f333e63
Prashant Joshi <prashant.joshi@softobiz.com>
2021-05-24 20:56:13 +0530

    initial commit added four files

---
+++ javascript/comparisonOperators.js
@@ -0,0 +1,26 @@
+document.write("look in the console");
+let v1 = 5;
+let v2 = 6;
+let v3 = '5';
+
+console.log(v1 == v2?'v1 and v2 equal':'v1 and v2 not equal');
+console.log(v1 == v3?'v1 and v3 equal':'v1 and v3 not equal');
+console.log(v1 === v2?'v1 and v2 equal':'v1 and v2 not equal');
+console.log(v1 === v3?'v1 and v3 equal':'v1 and v3 not equal');
+console.log(v1 != v2?'v1 and v2 equal':'v1 and v2 not equal');
+console.log(v1 != v3?'v1 and v3 equal':'v1 and v3 not equal');
+console.log(v1 !== v2?'v1 and v2 equal':'v1 and v2 not equal');
+console.log(v1 !== v3?'v1 and v3 equal':'v1 and v3 not equal');
+console.log(v1 > v2?'v1 greater then v2':'v1 less then v2');
+console.log(v1 >= v2?'v1 greater then equal to v2':'v1 less then equal too v2');
+console.log(v1 < v2?'v1 less v2':'v1 greater v2');
+console.log(v1 <= v2?'v1 less then equal too v2':'v1 greater then equal v2');
+console.log(v1 < v3?'v1 less v3':'v1 greater v3');
+console.log(v1 > v3?'v1 greater v3':'v1 less v3');
+
+console.log('v1 = ' + typeof(v1) + v1);
+console.log('v2 = ' + typeof(v2) + v2);
+console.log('v3 = ' + typeof(v3) + v3);
+const a = "hello";
+const b = 5;
+console.log(a/b);
