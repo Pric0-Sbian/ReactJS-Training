6729f6c20baa2b08216cffa585bf89b2e4fe4667
Prashant Joshi <prashant.joshi@softobiz.com>
2021-06-18 17:13:53 +0530

    React app added project name is first-project

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,15 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import CompProps from './myComponents/CompProps';
+import Blogs from './myComponents/Blogs';
+//import logo from './logo.svg';
+//import ReactDOM from 'react-dom';
 
-function App() {
+// React.createElement(
+//   'h1',
+//   {className: 'name'}
+// );
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// const userDefined = <Welcome name="Sara" />;
+// ReactDOM.render(
+//   userDefined,
+//   document.getElementsByClassName('name')
+// );
+function App() { 
+    // const element = <p><i>this is a html tag passed in a react element</i></p>;
+    // ReactDOM.render(element, document.getElementById("root"));
   return (
-    <div className="App">
+    <>
+    <CompProps />
+    <Blogs />
+    </>
+  );
+}
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('new'));
+// }
+
+// setInterval(tick, 1000);
+
+export default App;
+/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br />If I Write my name here and save will the name Show somewhere<br />
-          <i>Prashant Joshi</i>
         </p>
         <a
           className="App-link"
@@ -20,8 +56,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+    </div> */
